a06bfa079b763fcabf4da97b2294be9a3c98d3b5
NickW16 <171855220+NickW16@users.noreply.github.com>
2024-09-18 14:21:39 -0300

    finish getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log ("Hello World");
+console.log ("This is a Rock Paper Scissors Game");
+
+/* This section randomizes and shows a CPU choice */
+function getComputerChoice (choice) {
+    let CPUresult = Math.floor(Math.random() * 3);
+    if (CPUresult === 0)  {
+        CPUresult = 'Rock';
+    }
+    if (CPUresult === 1) {
+        CPUresult = 'Paper';
+    }
+    if (CPUresult === 2) {
+        CPUresult = 'Scissors';
+    }
+    return CPUresult;
+
+}
+console.log(getComputerChoice());
+
+/* This section takes the human choice */
+function getHumanChoice (choice) {
+    
+}
