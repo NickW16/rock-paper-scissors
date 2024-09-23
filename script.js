2ca21507d4a29310783528e166278ae62c909d15
NickW16 <171855220+NickW16@users.noreply.github.com>
2024-09-23 10:33:40 -0300

    Finish logic to play a round

--- script.js
+++ script.js
@@ -12,32 +12,54 @@ function getComputerChoice (choice) {
     if (CPUresult === 2) {
         CPUresult = 'Scissors';
     }
+    console.log("The Computer chose:", CPUresult);
     return CPUresult;
-
 }
-console.log(getComputerChoice());
 
 /* This section takes the human choice */
 let humanChoice = prompt("Rock, paper or scissors?");
-
 function getHumanChoice (choice) {
-    if (humanChoice === "Rock" || "rock") {
+    if (humanChoice.toLowerCase() === "rock") {
         humanChoice = "Rock";
     }
-    if (humanChoice === "Paper" || "paper") {
+    if (humanChoice.toLowerCase() === "paper") {
         humanChoice = "Paper";
     }
-    if (humanChoice === "Scissors" || "scissors") {
+    if (humanChoice.toLowerCase() === "scissors") {
         humanChoice = "Scissors";
     }
+    console.log("You chose:", humanChoice);
     return humanChoice;
 }
-console.log(getHumanChoice());
 
 /* This section keeps track of player's score */
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound (play) {
-    
+/* This section plays a round */
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log("You lose! Paper beats Rock")
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("You lose! Scissors beats Paper")
+    }
+    else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        console.log("You lose! Rock beats Scissors")
+    }
+    else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You Win! Scissors beats Paper")
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("You Win! Paper beats Rock")
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You Win! Rock beats Scissors")
+    }
+    else (console.log("It's a Draw! Play again"))
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
